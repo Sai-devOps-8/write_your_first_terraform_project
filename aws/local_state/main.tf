f9ce530ae6b22a4a950a6b44d499af14e820f1eb
root <root@ip-172-31-0-181.ap-south-1.compute.internal>
2025-01-11 22:40:33 +0000

    terraformsetup

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,13 +1,22 @@
 terraform {
+  required_version = ">= 1.2.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
 
-  required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket         = "024848450159-terraform-states"
+    key            = "development/service-name.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
+
 
 provider "aws" {
   region  = "us-west-2"
@@ -21,4 +30,38 @@ resource "aws_instance" "app_server" {
     Name = "Terraform_Demo"
   }
 }
+data "aws_caller_identity" "current" {}
 
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # With account id, this S3 bucket names can be *globally* unique.
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
